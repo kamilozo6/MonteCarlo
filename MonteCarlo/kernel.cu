f731918395a5ebb17c8414b58aa5389a9efc73e0
kamilozo6 <s160829@student.pg.gda.pl>
2019-05-20 03:12:12 +0200

    Add monte carlo

--- MonteCarlo/kernel.cu
+++ MonteCarlo/kernel.cu
@@ -1,19 +1,23 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include "Vector.cuh"
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+#include <curand_kernel.h>
 
 #include <stdio.h>
+#include <time.h>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+__global__ void addKernel(int* c, const int* a, const int* b)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
 }
 
-__device__ void ChangeState(int result, int & yes, int & no, int & unknown)
+__device__ void ChangeState(int result, int& yes, int& no, int& unknown)
 {
 	switch (result)
 	{
@@ -41,7 +45,7 @@ __device__ void ChangeState(int result, int & yes, int & no, int & unknown)
 	}
 }
 
-__device__ int CountSize(int n)
+int CountSize(int n)
 {
 	int size = 0;
 	for (int i = 1; i <= n + 1; i++)
@@ -51,9 +55,9 @@ __device__ int CountSize(int n)
 	return size;
 }
 
-__device__ int EvaluateCase(int yes, int no, int unknown, int n)
+__device__ int EvaluateCase(int yes, int no, int unknown, int n, curandState* randState)
 {
-	int randomValue = rand() % n + 1;
+	int randomValue = (int)(curand_uniform(&randState[0]) * n) % n + 1;
 	// 0 yes, 1 no, 2 unknown
 	int firstSelection;
 	int secondSelection;
@@ -74,7 +78,7 @@ __device__ int EvaluateCase(int yes, int no, int unknown, int n)
 	}
 
 	n--;
-	randomValue = rand() % n + 1;
+	randomValue = (int)(curand_uniform(&randState[0]) * n) % n + 1;
 	if (randomValue <= yes)
 	{
 		secondSelection = 0;
@@ -94,32 +98,81 @@ __device__ int EvaluateCase(int yes, int no, int unknown, int n)
 	return result;
 }
 
-__global__ MyMatrix* MonteCarlo(int n)
+__device__ void GetYesNoFromIndex(int index, int n, int& yes, int& no)
 {
-	int iterationNumber = 100000, size = CountSize(n);
-	MyMatrix *X = Vector::Create(size);
-	MyMatrix *numerator = Vector::Create(size);
-	MyMatrix *denominator = Vector::Create(size);
-	bool *casesUsed = new bool[size];
-	for (int i = 0; i < iterationNumber; i++)
+	int currentIndex = n;
+	int iterator = 1;
+	if (index <= n)
 	{
+		yes = 0;
+		no = index;
+		return;
+	}
+
+	while ((currentIndex + n + 1 - iterator) < index)
+	{
+		currentIndex += n + 1 - iterator;
+		iterator++;
+	}
+	yes = iterator;
+	no = index - currentIndex - 1;
+}
+
+__device__ int ReturnIndex(int yes, int no, int n)
+{
+	int index = 0;
+
+	for (int i = 1; i <= yes; i++)
+	{
+		index += n + 2 - i;
+	}
+
+	index += no;
+	return index;
+}
+
+extern __shared__ int shared[];
+__global__ void MonteCarlo(int* outNumerators, int* outDenominators, int states, int peoples, int iterationsNum, int iterationsPerThread)
+{
+	int* numerator = (int*)shared;
+	int* denominator = (int*)& shared[states];
+	bool* casesUsed = new bool[states]();
+	int tId = threadIdx.x + (blockIdx.x * blockDim.x);
+	curandState randState;
+	curand_init((unsigned long long)clock() + tId, 0, 0, &randState);
+
+	// Set shared memory to 0
+	for (int i = threadIdx.x; i < states; i += blockDim.x)
+	{
+		numerator[i] = 0;
+		denominator[i] = 0;
+	}
+
+	__syncthreads();
+
+	for (int i = 0; i < iterationsNum; i++)
+	{
+		/*if (iterationsNum <= iterationsPerThread * tId + i)
+		{
+			break;
+		}*/
 		bool end = false;
 		bool isYesResult = false;
-		int test = rand() % size;
-		int yes = n / 2, no = n / 2, unknown = n - yes - no;
-        Vector::GetYesNoFromIndex(test, n, yes, no);
-		unknown = n - yes - no;
-		for (int j = 0; j < size; j++)
+		int test = (int)(curand_uniform(&randState) * states) % states;
+		int yes = peoples / 2, no = peoples / 2, unknown = peoples - yes - no;
+		GetYesNoFromIndex(test, peoples, yes, no);
+		unknown = peoples - yes - no;
+		for (int j = 0; j < states; j++)
 		{
 			casesUsed[j] = false;
 		}
-		casesUsed[Vector::ReturnIndex(yes, no, n)] = true;
+		casesUsed[ReturnIndex(yes, no, peoples)] = true;
 		while (!end)
 		{
-			int result = EvaluateCase(yes, no, unknown, n);
+			int result = EvaluateCase(yes, no, unknown, peoples, &randState);
 			ChangeState(result, yes, no, unknown);
 
-			int index = Vector::ReturnIndex(yes, no, n);
+			int index = ReturnIndex(yes, no, peoples);
 			casesUsed[index] = true;
 
 			if (yes == 0 || no == 0)
@@ -131,137 +184,182 @@ __global__ MyMatrix* MonteCarlo(int n)
 				end = true;
 			}
 		}
-		for (int j = 0; j < size; j++)
+		for (int j = tId; j < states; j+=blockDim.x)
 		{
 			if (casesUsed[j])
 			{
 				if (isYesResult)
 				{
-					numerator->matrix[j][0]++;
+					numerator[j]++;
 				}
-				denominator->matrix[j][0]++;
+				denominator[j]++;
 			}
 		}
 	}
 	delete casesUsed;
-	for (int i = 0; i < size; i++)
+	__syncthreads();
+	for (int i = threadIdx.x; i < states; i += blockDim.x)
 	{
-		if (denominator->matrix[i][0] != 0)
-		{
-			X->matrix[i][0] = numerator->matrix[i][0] / denominator->matrix[i][0];
-		}
+		outNumerators[i] = numerator[i];
+		outDenominators[i] = denominator[i];
 	}
+}
 
-	delete numerator;
-	delete denominator;
-	return X;
+__global__ void CalculateProbabilities(int* numerators, int* denominators, double* winProbabilities, int states)
+{
+	int tId = threadIdx.x + (blockIdx.x * blockDim.x);
+	if (tId < states)
+	{
+		if (denominators[tId] != 0)
+			winProbabilities[tId] = numerators[tId] * 1.0 / denominators[tId];
+		else
+			winProbabilities[tId] = 0;
+	}
 }
 
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	// Monte carlo init numbers
+	const int iterationsNumber = 1000000;
+	const int peoplesNumber = 5;
+	const int statesNumber = CountSize(peoplesNumber);
+	double* winProbabilities = new double[statesNumber];
+	// Threads and blocks
+	const int threadsNumber = 256;
+	int blocksNumber = 1;
+	// Cuda variables
+	double* cu_winProbabilities;
+	int* cu_statesNumerators;
+	int* cu_statesDenominators;
+	int simulationsPerThread = ceil(iterationsNumber * 1.0 / threadsNumber);
+	cudaError_t cudaStatus;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	// Allocate memory on gpu
+	cudaStatus = cudaMalloc((void**)& cu_winProbabilities, statesNumber * sizeof(double));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to allocate (error code %s)!\n", cudaGetErrorString(cudaStatus));
+		exit(EXIT_FAILURE);
+	}
+	cudaStatus = cudaMalloc((void**)& cu_statesNumerators, statesNumber * sizeof(int));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to allocate (error code %s)!\n", cudaGetErrorString(cudaStatus));
+		exit(EXIT_FAILURE);
+	}
+	cudaStatus = cudaMalloc((void**)& cu_statesDenominators, statesNumber * sizeof(int));
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to allocate (error code %s)!\n", cudaGetErrorString(cudaStatus));
+		exit(EXIT_FAILURE);
+	}
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	// Set numerators and denominators values to 0
+	cudaMemset(cu_statesNumerators, 0, statesNumber * sizeof(int));
+	cudaMemset(cu_statesDenominators, 0, statesNumber * sizeof(int));
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	// Monte Carlo simulation
+	MonteCarlo << <blocksNumber, threadsNumber, 2 * statesNumber * sizeof(int) >> > (cu_statesNumerators, cu_statesDenominators, statesNumber, peoplesNumber, iterationsNumber, simulationsPerThread);
 
-    return 0;
+	// Calculate states probabilities to win
+	blocksNumber = (statesNumber + threadsNumber - 1) / threadsNumber;
+	CalculateProbabilities << <blocksNumber, threadsNumber >> > (cu_statesNumerators, cu_statesDenominators, cu_winProbabilities, statesNumber);
+
+	// Copy results to host
+	cudaMemcpy(winProbabilities, cu_winProbabilities, statesNumber * sizeof(double), cudaMemcpyDeviceToHost);
+
+	for (int i = 0; i < statesNumber; i++)
+	{
+		printf("%.5f\n", winProbabilities[i]);
+	}
+
+	// Free memory
+	cudaFree(cu_winProbabilities);
+	cudaFree(cu_statesNumerators);
+	cudaFree(cu_statesDenominators);
+
+	delete winProbabilities;
+
+	return 0;
 }
 
 // Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	int* dev_a = 0;
+	int* dev_b = 0;
+	int* dev_c = 0;
+	cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	// Allocate GPU buffers for three vectors (two input, one output)    .
+	cudaStatus = cudaMalloc((void**)& dev_c, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	cudaStatus = cudaMalloc((void**)& dev_a, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	cudaStatus = cudaMalloc((void**)& dev_b, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	// Copy input vectors from host memory to GPU buffers.
+	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+	// Launch a kernel on the GPU with one thread for each element.
+	addKernel << <1, size >> > (dev_c, dev_a, dev_b);
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
 
 Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	cudaFree(dev_c);
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+
+	return cudaStatus;
 }
