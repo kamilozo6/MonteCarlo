8285e28e9f96b28b0814983521c97f9c922d2473
jakubjab <jakub.jablonski@intel.com>
2019-05-21 09:17:49 +0200

    Changed name of main source file

--- MonteCarlo/compile.sh
+++ MonteCarlo/compile.sh
@@ -2,6 +2,6 @@ rm kernel.o
 rm main.o
 rm a.out
 
-mpicxx -c main.cpp -o main.o
+mpicxx -c mpi.cpp -o main.o
 nvcc -c kernel.cu -o kernel.o -std=c++11
 mpicxx main.o kernel.o -lcudart -L/usr/local/cuda/lib64
