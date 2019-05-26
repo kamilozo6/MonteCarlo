2e06781c3174c385528e9c185c235d238849a8ef
jakubjab <jakub.jablonski@intel.com>
2019-05-26 21:20:17 +0200

    Recovering wywalona funkcja

--- MonteCarlo/kernel.cu
+++ MonteCarlo/kernel.cu
@@ -188,13 +188,64 @@ __global__ void MonteCarloOpt(double* winProbabilities, int states, int peoples,
 		}
 		if (isYesResult)
 		{
-			outNumerators[tId] ++;
+			s[threadIdx.x] += 1.0;
 		}
-		outDenominators[tId] ++;
+	}
+	__syncthreads();
+
+	if (threadIdx.x == 0)
+	{
+		for (int i = 0; i < blockDim.x; i++)
+		{
+			winProbabilities[blockIdx.x] += s[i];
+		}
+		winProbabilities[blockIdx.x] /= iterationsNum;
 	}
 }
 
-__global__ void MonteCarloOpt(double* winProbabilities, int states, int peoples, int iterationsNum)
+__global__ void MonteCarloSeq(double* winProbabilities, int states, int peoples, int iterationsNum)
+{
+	// Init random numbers generator
+	curandState randState;
+	curand_init((unsigned long long)clock(), 0, 0, &randState);
+	for (int st = 0; st < states; st++)
+	{
+		for (int i = 0; i < iterationsNum; i++)
+		{
+			bool end = false;
+			bool isYesResult = false;
+			// Begining state
+			int test = st;
+			int yes = peoples / 2, no = peoples / 2, unknown = peoples - yes - no;
+			// Get yes, no numbers according to state
+			GetYesNoFromIndex(test, peoples, yes, no);
+			unknown = peoples - yes - no;
+			while (!end)
+			{
+				int result = EvaluateCase(yes, no, unknown, peoples, &randState);
+				ChangeState(result, yes, no, unknown);
+
+				// If yes == 0 there is no chance to "win"
+				// If no == 0 there is no chance to "lose"
+				if (yes == 0 || no == 0)
+				{
+					if (yes > 0)
+					{
+						isYesResult = true;
+					}
+					end = true;
+				}
+			}
+			if (isYesResult)
+			{
+				winProbabilities[st] += 1.0;
+			}
+		}
+		winProbabilities[st] /= iterationsNum;
+	}
+}
+
+__global__ void MonteCarlo(double* winProbabilities, int states, int peoples, int iterationsNum)
 {
 	int tId = threadIdx.x + (blockIdx.x * blockDim.x);
 	if (tId >= states)
