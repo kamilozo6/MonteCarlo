f23d17f2d5140aa3db17af058898b351e1998271
jakubjab <jakub.jablonski@intel.com>
2019-05-27 18:21:44 +0200

    changed compiled file destination to home/student folder

--- MonteCarlo/compile.sh
+++ MonteCarlo/compile.sh
@@ -4,4 +4,4 @@ rm a.out
 
 mpicxx -std=c++11 -c mpi.cpp -o main.o
 nvcc -c kernel.cu -o kernel.o -std=c++11
-mpicxx -std=c++11 main.o kernel.o -lcudart -L/usr/local/cuda/lib64
+mpicxx -std=c++11 main.o kernel.o -lcudart -L/usr/local/cuda/lib64 -o /home/student/a.out
